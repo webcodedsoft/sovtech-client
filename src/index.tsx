2db1a0562b2570c8ca0b182a553eca74a1e4e743
Adedeji Sikiru A <adedejisikiruadesola@gmail.com>
2021-10-24 16:51:39 +0100

    Change server endpoint

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/api/v1',
+  uri: 'https://sovtech-challenge-backend.herokuapp.com/api/v1',
   cache: new InMemoryCache(),
 });
 
